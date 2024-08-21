66ffeda1e1fd9561a0b7d96bad149c599a7de904
DESKTOP-MBN6FJJ\Admin <nguyenloi.itse@gmail.com>
2024-08-21 21:29:12 +0700

    Implement Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './exception/http-exception-filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Configure Swagger documentation for the API
+  const config = new DocumentBuilder()
+    .setTitle('Location API')
+    .setDescription('The Location API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
